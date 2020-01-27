10273784fa3ad7f7c9067e9310dfed88e9287d75
Kai Page <kaibug@gmail.com>
2020-01-27 11:43:15 -0600

    Basic layout scaffolding

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -1,4 +1,5 @@
 import 'package:angular/angular.dart';
+import 'package:angular_components/angular_components.dart';
 
 // AngularDart info: https://angulardart.dev
 // Components info: https://angulardart.dev/components
@@ -6,6 +7,14 @@ import 'package:angular/angular.dart';
 @Component(
   selector: 'dl-app',
   templateUrl: 'app_component.html',
-  styleUrls: ['app_component.css'],
+  styleUrls: [
+    'app_component.css',
+    'package:angular_components/app_layout/layout.scss.css',
+  ],
+  directives: [
+    MaterialButtonComponent,
+    MaterialTemporaryDrawerComponent,
+    MaterialIconComponent,
+  ],
 )
 class AppComponent {}
