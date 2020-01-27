5ac6c09239137841dc5313c86a487f579ad6fb44
Kai Page <kaibug@gmail.com>
2020-01-27 11:13:56 -0600

    Init commit

---
+++ lib/app_component.dart
@@ -0,0 +1,11 @@
+import 'package:angular/angular.dart';
+
+// AngularDart info: https://angulardart.dev
+// Components info: https://angulardart.dev/components
+
+@Component(
+  selector: 'dl-app',
+  templateUrl: 'app_component.html',
+  styleUrls: ['app_component.css'],
+)
+class AppComponent {}
